ed556f9679fdb3a3fd7d9e9e3ac3a8af22413fc8
Théo Delaporte <theodelaporte@MacBook-de-Theo.local>
2022-05-02 16:36:54 +0200

    added QuilJs

--- templates/Admin/Blogs/add.php
+++ templates/Admin/Blogs/add.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Blog $blog
@@ -13,20 +14,4 @@
 <li><?= $this->Html->link(__('New Tag'), ['controller' => 'Tags', 'action' => 'add'], ['class' => 'nav-link']) ?></li>
 <?php $this->end(); ?>
 <?php $this->assign('tb_sidebar', '<ul class="nav flex-column">' . $this->fetch('tb_actions') . '</ul>'); ?>
-
-<div class="blogs form content">
-    <?= $this->Form->create($blog) ?>
-    <fieldset>
-        <legend><?= __('Add Blog') ?></legend>
-        <?php
-            echo $this->Form->control('title');
-            echo $this->Form->control('slug');
-            echo $this->Form->control('summary');
-            echo $this->Form->control('published');
-            echo $this->Form->control('content');
-            echo $this->Form->control('tags._ids', ['options' => $tags]);
-                ?>
-    </fieldset>
-    <?= $this->Form->button(__('Submit')) ?>
-    <?= $this->Form->end() ?>
-</div>
+<?= $this->element('Admin/Blogs/form', ['title' => __('Add Blog')]) ?>
